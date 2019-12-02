b1b62460d6cb96a615ba2313f89f8cad4fae368e
Gabriel Costa <gabrielcosta08@gmail.com>
2019-12-02 01:12:09 -0300

    Create basic test for ListViewModel

---
+++ features/list/src/test/java/dev/tigrao/list/ListViewModelTest.kt
@@ -0,0 +1,33 @@
+package dev.tigrao.list
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import dev.tigrao.commons.statemachine.StateEvent
+import dev.tigrao.commons.statemachine.StateMachine
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import org.junit.Rule
+import org.junit.Test
+
+class ListViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun whenFetchSongs_ShouldEmitStateEvent() {
+        val useCase = mockk<ListUseCase>()
+        every { useCase.fetchSongs() } returns Observable.just(listOf())
+        val viewModel =
+            ListViewModel(useCase, StateMachine(Schedulers.trampoline()), Schedulers.trampoline())
+        val observer = mockk<Observer<in StateEvent<List<ListVO>>>>()
+        viewModel.liveData.observeForever(observer)
+
+        viewModel.fetchSongs()
+
+        verify { observer.onChanged(any()) }
+    }
+}
